79ba28b3f9548bd99e4dbe8402941012ddc4ddf8
sn4k321 <jyoris2124@gmail.com>
2022-11-20 22:26:28 -0300

    proyecto

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+let editButton = document.querySelector(".profile__edit-button");
+let closeIcon = document.querySelector(".popup__close-icon");
+let popup = document.querySelector(".popup");
+let nameInput = document.querySelector("#name");
+let descriptionInput = document.querySelector("#description");
+let formElement = document.querySelector(".form");
+let name = document.querySelector(".profile__name");
+let description = document.querySelector(".profile__job");
+
+function showPopup() {
+  nameInput.value = name.textContent;
+  descriptionInput.value = description.textContent;
+  popup.classList.add("popup_open");
+}
+
+function hidePopup() {
+  popup.classList.remove("popup_open");
+}
+
+function handleProfileFormSubmit(e) {
+  e.preventDefault();
+  name.textContent = nameInput.value;
+  description.textContent = descriptionInput.value;
+  hidePopup();
+}
+
+editButton.addEventListener("click", showPopup);
+closeIcon.addEventListener("click", hidePopup);
+formElement.addEventListener("submit", handleProfileFormSubmit);
